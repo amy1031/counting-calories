c5796f5fd21ac7acdf906262df1deab01f82bf09
Amy Roche <amy.roche31@gmail.com>
2017-05-18 15:11:27 -0600

    New Key

--- src/food-service.js
+++ src/food-service.js
@@ -1,7 +1,7 @@
 import $ from 'jquery'
 //Replace these with your own!!!
-const key = '8120134b8a361912934e53e415fffd2c'
-const id = '814a7db9'
+const key = '6667b800a0bda2bfb04d46aff1e8afc6'
+const id = '8ffe2f4c'
 
 const baseUrl = 'https://api.nutritionix.com/v1_1/search/'
 const fields = '?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat%2Cnf_total_carbohydrate%2Cnf_protein&appId='+id+'&appKey='+ key
